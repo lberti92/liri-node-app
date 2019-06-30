6fd8e9adc1fe1d18ab87cbf88dfa0d2149d2b9d2
Breeze <lberti@msn.com>
2019-06-29 22:25:05 -0600

    all aspects of liri.js is working

--- liri.js
+++ liri.js
@@ -1,90 +1,73 @@
 require("dotenv").config();
-var fs = require("fs");
 
 var keys = require("./keys.js");
 var axios = require("axios");
+var moment = require("moment");
+var fs = require("fs");
 
-// var activity = process.argv[2] || "bad";
-// var value = process.argv.slice(3).join("+");
-// console.log()
-
+var activity = process.argv[2] || "bad";
+var value = process.argv.slice(3).join("+");
 
 function movie() {
 
-  var movieName = process.argv.slice(3).join(" ");
-  var movieUrl = "http://www.omdbapi.com/?t=" + movieName + "&apikey=trilogy";
+  if (!value) {
+    value = "Mr. Nobody";
+  }
 
-  console.log(movieUrl);
+  var movieUrl = "http://www.omdbapi.com/?t=" + value + "&apikey=trilogy";
 
   axios.get(movieUrl).then(
     function (response) {
 
-      for (var i = 0; i < response.data.length; i++) {
-        console.log(response.data);
-        console.log("The movie's Title is: " + response.data.Title);
-        console.log("The movie'S Year is: " + response.data.Year);
-        console.log("The movie's imbd Rating is: " + response.data.imdbRating);
-        console.log("The movie's Country is: " + response.data.Country);
-        console.log("The movie's Language is: " + response.data.Language);
-        console.log("The movie's Plot is: " + response.data.Plot);
-        console.log("The movie's Actors are: " + response.data.Actors);
-        console.log("The movie's Rotten Tomatoes Rating is: " + response.data.Ratings[1].Value);
-      }
+      var jsonData = response.data;
+
+      var movieData = [];
+
+      movieData.push(
+        "Title: " + jsonData.Title,
+        "Year: " + jsonData.Year,
+        "IMDB Rating: " + jsonData.imdbRating,
+        "Rotten Tomatoes Rating: " + jsonData.Ratings[1].Value,
+        "Country: " + jsonData.Country,
+        "Language: " + jsonData.Language,
+        "Actor(s): " + jsonData.Actors,
+        "Plot: " + jsonData.Plot
+      )
+
+      fs.appendFile("log.txt", movieData.join(" "), function (err) {
+        if (err) throw err;
+      })
+      console.log(movieData.join("\n"));
     })
-    .catch(function (error) {
-      if (error.response) {
-        console.log("---------------Data---------------");
-        console.log(error.response.data);
-        console.log("---------------Status---------------");
-        console.log(error.response.status);
-        console.log("---------------Status---------------");
-        console.log(error.response.headers);
-      } else if (error.request) {
-        console.log(error.request);
-      } else {
-        console.log("Error", error.message);
-      }
-      console.log(error.config);
+    .catch(function (err) {
+      console.log(err);
     });
 }
 
 function artist() {
-  var artist = process.argv.slice(3).join(" ");
 
-  var concertUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+  var concertUrl = "https://rest.bandsintown.com/artists/" + value + "/events?app_id=codingbootcamp";
 
   axios.get(concertUrl).then(
     function (response) {
-      // console.log(response.data);
-      //create a for loop
-      for (var i = 0; i < response.data.length; i++) {
-        console.log("The name of Venue is: " + response.data[i].venue.name);
-        console.log("The Venue location is: " + response.data[i].venue.city);
-        console.log("The Venue location is: " + response.data[i].venue.region);
-        console.log("The Venue location is: " + response.data[i].venue.country);
 
-        // console.log("The data of event is: " + moment(response.data.datetime).format("dddd, MMMM Do YYYY, h:mm:ss a"));
-        console.log("The date of event is: " + response.data[i].datetime);
+      var jsonData = response.data;
 
-        // moment().format("dddd, MMMM Do YYYY, h:mm:ss a")
-        // moment(response.data.datetime).format("dddd, MMMM Do YYYY, h:mm:ss a")     
+      for (var i = 0; i < jsonData.length; i++) {
+
+        var concertData = [
+          jsonData[i].venue.city + ", " + jsonData[i].venue.region + " " + jsonData[i].venue.country + " at " +
+          jsonData[i].venue.name + " " + moment(jsonData[i].datetime).format("dddd, MMMM Do YYYY, h:mm:ss a")
+        ].join("\n");
+
+        fs.appendFile("log.txt", concertData, function (err) {
+          if (err) throw err;
+        })
+        console.log(concertData);
       }
     })
-    .catch(function (error) {
-      if (error.response) {
-        console.log("---------------Data---------------");
-        console.log(error.response.data);
-        console.log("---------------Status---------------");
-        console.log(error.response.status);
-        console.log("---------------Status---------------");
-        console.log(error.response.headers);
-      } else if (error.request) {
-        console.log(error.request);
-      } else {
-        console.log("Error", error.message);
-      }
-      console.log(error.config);
-
+    .catch(function (err) {
+      console.log(err);
     });
 }
 
@@ -92,20 +75,37 @@ function music() {
   var Spotify = require('node-spotify-api');
 
   var spotify = new Spotify(keys.spotify);
-  var song = process.argv.slice(2).join(" ");
+  var value = process.argv.slice(3).join(" ");
 
+  if (!value) {
+    value = "The Sign";
+  }
   spotify
-    .search({ type: 'track', query: song, limit: 10 })
+    .search({ type: 'track', query: value, limit: 1 })
     .then(function (response) {
-      // console.log(response.tracks.items);
-      for (var i = 0; i < response.tracks.items.length; i++) {
-        console.log(response.tracks.items[i].name);
-        console.log(response.tracks.items[i].album.external_urls.spotify);
-        console.log(response.tracks.items[i].album.name);
-        for (var j = 0; j < response.tracks.items[i].album.artists.length; j++) {
-          console.log(response.tracks.items[i].album.artists[j].name);
-          console.log("===================================================");
+
+      var jsonData = response.tracks.items;
+      var divider = "\n------------------------------------------------------------\n\n";
+
+      for (var i = 0; i < jsonData.length; i++) {
+
+        var artistData = [];
+
+        for (var j = 0; j < jsonData[i].album.artists.length; j++) {
+          artistData.push(jsonData[i].album.artists[j].name);
         }
+
+        var songData = [];
+        songData.push(
+          "Artist(s): " + artistData.join(", "),
+          "Song Name: " + jsonData[i].name,
+          "Preview song: " + jsonData[i].album.external_urls.spotify,
+          "Album: " + jsonData[i].album.name,
+        )
+        fs.appendFile("log.txt", songData.join(" ") + divider, function (err) {
+          if (err) throw err;
+        })
+        console.log(songData.join("\n") + divider);
       }
     })
     .catch(function (err) {
@@ -118,29 +118,30 @@ function doAnything() {
     if (err) {
       return console.log(err)
     }
-    console.log(data);
+    var dataArr = data.split(",");
+    activity = dataArr[0];
+    value = dataArr[1];
+
+    switchFunc(activity, value);
   })
-
-
-
 }
-// function switchFunc(activity, value) {
-switch (activity.toLowerCase()) {
-  case "movie-this":
-    console.log("movie");
-    movie(value);
-    break;
-  case "concert-this":
-    artist(value);
-    break;
-  case "spotify-this-song":
-    music(value);
-    break;
-  case "do-what-it-says":
-    doAnything();
-    break;
-  default:
-    return console.log("Invalid request");
+
+function switchFunc(action, item) {
+  switch (action.toLowerCase()) {
+    case "movie-this":
+      movie(item);
+      break;
+    case "concert-this":
+      artist(item);
+      break;
+    case "spotify-this-song":
+      music(item);
+      break;
+    case "do-what-it-says":
+      doAnything();
+      break;
+    default:
+      return console.log("Invalid request");
+  }
 }
-// }
-// switchFunc(action, amount);
+switchFunc(activity, value);
